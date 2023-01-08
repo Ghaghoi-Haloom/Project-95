54a2c529f676b2337f19d82897463a77ae65621d
Ghaghoi-Haloom <121165315+Ghaghoi-Haloom@users.noreply.github.com>
2023-01-08 21:21:02 +0530

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAEvU5PyQqy-PhJyyNFhZ6Rml26mQQYf0c",
+    authDomain: "don-bosco-liluah.firebaseapp.com",
+    databaseURL: "https://don-bosco-liluah-default-rtdb.firebaseio.com",
+    projectId: "don-bosco-liluah",
+    storageBucket: "don-bosco-liluah.appspot.com",
+    messagingSenderId: "860763981948",
+    appId: "1:860763981948:web:d68e1ffceb2e3a0a9521f9"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
